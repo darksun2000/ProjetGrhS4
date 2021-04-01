c1d61d413f04c585c3bae1eb5179470ada9960cb
khalid <khalid@DESKTOP-703JQ8P>
2021-04-01 13:29:19 +0100

    projet gestion des ressources humaines

--- ProjetGrhS4/src/web/ServletAdmin.java
+++ ProjetGrhS4/src/web/ServletAdmin.java
@@ -1,9 +1,8 @@
 package web;
 
-import java.io.File;
+
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.List;
 
 import javax.servlet.ServletException;
